5bf8643928f51059d69b6cfca07c4328db333896
byrmylz <bayramyilmaz061@gmail.com>
2024-10-22 10:54:20 +0200

    Layout dialect and Live reload dev tools added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    id("application")
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
 }
@@ -13,18 +14,25 @@ java {
     }
 }
 
+application {
+    mainClass.set("be.kdg.event.EventApplication")  // Specify the fully qualified name of the class with the main method
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation ("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.2.0")
+    compileOnly("org.projectlombok:lombok")
     implementation("org.springframework.boot:spring-boot-starter-web")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql:42.7.2")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
 
 
